0b54ed1710c6c235a31a75ab80afffc16a7afe2f
mayi-mama <mayimuro8845@gmail.com>
2023-03-28 23:16:20 +0900

    css, jQuery追記

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
 $(function(){
+    var loading = $("#js-loading");
+
+/*ページの読み込みが完了後にアニメーションを非表示・MVの高さを調整*/
+    $(window).on("load", function(){
+        var windowHeight = $(window).height();
+        $(".mv").height(windowHeight);
+        loading.delay("1000").fadeOut("2000");
+    });
+
+/*ページが読み込まれなくても非表示*/
+    setTimeout(function(){
+        loading.fadeOut("3000");
+    }, 8000);
+
+/*画面リサイズ時にMVの高さを調整*/
+$(window).resize(function () {
+    var windowHeight = $(window).height();
+    $(".mv").height(windowHeight);
+});
+
 
 });
