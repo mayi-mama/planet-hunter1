eec11079c7dcd17f5498743c28685f9b59cbc231
mayi-mama <mayimuro8845@gmail.com>
2023-04-10 23:03:52 +0900

    javascript

--- js/script-1.js
+++ js/script-1.js
@@ -22,5 +22,19 @@ window.addEventListener("resize", () => {
 }, false);
 
 //音声を再生と停止
+const music = new Audio('assets/audio/cinematic-melody-main-9785.mp3');
+const play = document.querySelector('#js-audio-play');
+
+// 再生ボタン
+play.addEventListener('click', function(){
+  if(!music.paused){
+    music.pause();
+  }else{
+    music.play();
+    music.loop = true;
+  }
+});
+
+
 
 
